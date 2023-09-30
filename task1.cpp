05ae9975a37ac56174b86eab90cc6ea1be7c5dd9
MuhammadMuhammadalif <144408964+MuhammadMuhammadalif@users.noreply.github.com>
2023-09-30 19:55:55 +0500

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+main()
+{
+int one,two;
+cout << "Enter the first number: ";
+cin >> one;
+cout << "Enter the second number: ";
+cin >> two;
+	if(one==two){
+	cout << "true";
+	}
+	if(one!=two){
+	cout << "false";
+	}
+}
+
+
